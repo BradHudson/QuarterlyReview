ba972ad17cd590d1f22e2fa19c7fb2a7a2e2b31e
Brad Hudson <bradrayhudson@gmail.com>
2017-06-29 14:23:44 -0400

    tweak controller for github response

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,17 @@ class SessionsController < ApplicationController
     # redirect_to '/'
     auth_hash = request.env['omniauth.auth']
 
-    render :text => auth_hash.inspect
+    binding.pry
+    @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    if @authorization
+      render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
+    else
+      user = User.new :name => auth_hash["info"]["nickname"], :email => auth_hash["info"]["email"]
+      user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+      user.save
+
+      render :text => "Hi #{user.name}! You've signed up."
+    end
   end
 
   def failure
