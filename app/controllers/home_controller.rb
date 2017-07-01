427895634a2e379227ba1f433daa44116d5bfcc6
Brad Hudson <bradrayhudson@gmail.com>
2017-07-01 13:43:17 -0400

    getting list of PRS :D

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,23 +2,39 @@ class HomeController < ApplicationController
   def index
     if session[:current_user].present?
       repos = {}
-      binding.pry
-      token = session[:current_user]["token"]
+      @token = session[:current_user]["token"]
       name = session[:current_user]["name"]
+
+      #get the organizations for the user
       orgs_uri = "https://api.github.com/users/#{ name }/orgs"
 
-      binding.pry
+
       @orgs= HTTParty.get(orgs_uri).parsed_response
-      @orgs.each do |org|
-        repos_uri = "https://api.github.com/orgs/#{ org }/repos?per_page=100"
-        repos[:org] = HTTParty.get(repos_uri).parsed_response
-      end
+
+      #loop through each organization and list the repos
+      # @orgs.each do |org|
+      #   repos_uri = "https://api.github.com/orgs/#{ org }/repos?per_page=100"
+      #   repos[:org] = HTTParty.get(repos_uri, headers: headers).parsed_response
+      # end
+
+      #once they select a repo, lets loop through the PR's for that repo
+
+      org = 'cbdr'
+      repo = 'consumer-main'
+      author = 'BradHudson'
+      date = '2017-05-01'
+      prs_uri = "https://api.github.com/search/issues?q=is:pr+repo:#{ org }/#{ repo }+author:#{ author }+merged:>#{ date }"
+      @pull_requests = HTTParty.get(prs_uri, headers: headers).parsed_response
     end
+
+    #search PRS
+    #
+    #https://developer.github.com/v3/search/#search-issues
   end
 
   private
 
   def headers
-    # :headers => { "Authorization" => "token #{ token }"}
+    { "Authorization" => "token #{ @token }", "User-Agent" => "Awesome-Octocat-App"}
   end
 end
